76ce8da4ce6bc4d39ef22fd7bae0f7b76f75201f
FizOkor <okorfavour2000@gmail.com>
2025-11-14 15:28:56 +0100

    next.js reconfigured

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,44 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Don't attempt to bundle these packages on the client
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+        stream: false,
+        path: false,
+        os: false,
+        util: false,
+        url: false,
+        'pino-pretty': false,
+        'thread-stream': false,
+        'worker_threads': false,
+      };
+
+      // Exclude problematic modules from client-side bundles
+      config.plugins.push(
+        new config.optimization.splitChunks.cacheGroups.constructor({
+          test: /[\\/]node_modules[\\/](thread-stream|pino)[\\/]/,
+          name: 'vendor-pino',
+          enforce: true,
+        })
+      );
+    }
+    
+    // Add externals for SSR
+    config.externals = [
+      ...(config.externals || []),
+      { 'thread-stream': 'commonjs thread-stream' },
+      { 'pino-pretty': 'commonjs pino-pretty' },
+    ];
+    
+    return config;
+  },
 }
 
-export default nextConfig
+export default nextConfig;
